15dd6d761383d95fbf9e3a3f8764673e74a8e288
Marcelo Monteiro <mamonteirogarcia@gmail.com>
2024-11-25 16:55:01 +0100

    Commit #4 "Affichage de la taille des variable"

--- Transgerbeur/Transgerbeur.cpp
+++ Transgerbeur/Transgerbeur.cpp
@@ -23,7 +23,7 @@ int main() {
 		SRouleau rouleauCasier;
 		UContenu contenu;
 		SCasier casier;
-		SCasier encasiers[2];
+		SCasier encasiers[9];
 
 	/*Casier 1*/
 
@@ -50,7 +50,7 @@ int main() {
 	/*Casier 2*/
 
 		SRouleau ro2;
-		ro2.longueur = 4.777;
+		ro2.longueur = 4.7;
 
 		SPalette pa2;
 		pa2.poids = 55;
@@ -69,6 +69,40 @@ int main() {
 		c2.px = 7;
 		c2.py = 8;
 
+		encasiers[0] = c1 ;
+		encasiers[1] = c2 ;
 
-		return 0;
+	cout << endl << " Voici la taille de pa1 : "  << sizeof(pa1) ;
+	cout << endl << " Voici la taille de pa1.poids : " << sizeof(pa1.poids) ;
+	cout << endl << " Voici la taille de pa1.reference : "  << sizeof(pa1.reference) ;
+	cout << endl << " Voici la taille de ro1 : " << sizeof(ro1) ;
+	cout << endl << " Voici la taille de ro1.longueur : "  << sizeof(ro1.longueur) ;
+	cout << endl << " Voici la taille de etatC1 : " << sizeof(etatC1) ;
+	cout << endl << " Voici la taille de cont1 : " << sizeof(cont1) ;
+	cout << endl << " Voici la taille de cont1.paletteCasier : " << sizeof(cont1.paletteCasier) ;
+	cout << endl << " Voici la taille de cont1.rouleauCasier : " << sizeof(cont1.rouleauCasier) ;
+	cout << endl << " Voici la taille de c1 : " << sizeof(c1) ;
+	cout << endl << " Voici la taille de c1.etat : " << sizeof(c1.etat) ;
+	cout << endl << " Voici la taille de c1.paletteCasier : " << sizeof(c1.paletteCasier) ;
+	cout << endl << " Voici la taille de c1.rouleauCasier : " << sizeof(c1.rouleauCasier) ;
+	cout << endl << " Voici la taille de c1.px : " << sizeof(c1.px) ;
+	cout << endl << " Voici la taille de c1.py : " << sizeof(c1.py) << endl;
+ 
+	cout << endl << " Voici la taille de pa2 : " << sizeof(pa2);
+	cout << endl << " Voici la taille de pa2.poids : " << sizeof(pa2.poids);
+	cout << endl << " Voici la taille de pa2.reference : " << sizeof(pa2.reference);
+	cout << endl << " Voici la taille de ro2 : " << sizeof(ro2);
+	cout << endl << " Voici la taille de ro2.longueur : " << sizeof(ro2.longueur);
+	cout << endl << " Voici la taille de etatC2 : " << sizeof(etatC2);
+	cout << endl << " Voici la taille de cont2 : " << sizeof(cont2);
+	cout << endl << " Voici la taille de cont2.paletteCasier : " << sizeof(cont2.paletteCasier);
+	cout << endl << " Voici la taille de cont2.rouleauCasier : " << sizeof(cont2.rouleauCasier);
+	cout << endl << " Voici la taille de c2 : " << sizeof(c2);
+	cout << endl << " Voici la taille de c2.etat : " << sizeof(c2.etat);
+	cout << endl << " Voici la taille de c2.paletteCasier : " << sizeof(c2.paletteCasier);
+	cout << endl << " Voici la taille de c2.rouleauCasier : " << sizeof(c2.rouleauCasier);
+	cout << endl << " Voici la taille de c2.px : " << sizeof(c2.px);
+	cout << endl << " Voici la taille de c2.py : " << sizeof(c2.py) << endl;
+
+	return 0;
 }
