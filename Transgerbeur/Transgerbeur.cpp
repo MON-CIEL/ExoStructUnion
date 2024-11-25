070ed96177833f487c5d8ed35410f163b0d2dcdb
Marcelo Monteiro <mamonteirogarcia@gmail.com>
2024-11-25 16:35:39 +0100

    Commit #3  "Programme Test"

--- Transgerbeur/Transgerbeur.cpp
+++ Transgerbeur/Transgerbeur.cpp
@@ -1,6 +1,6 @@
 #include <stdlib.h> /* Lib Standard*/
 #include <iostream> /* Lib Cin , Cout */
-#include <string>
+#include <string.h>
 #include "Transgerbeur.h"
 using namespace std;
 
@@ -14,19 +14,61 @@ using namespace std;
 
 
 int main() {
+
 	cout << endl << "Voici le porgramme Transgerbeur" << endl;
 
 	/* Declarations Variables */
-		int px;
-		int py;
-		int poids;
-		char reference;
-		float longueur;
-		int numero;
 		EEtat etat;
-		UContenu contenu;
 		SPalette paletteCasier;
 		SRouleau rouleauCasier;
+		UContenu contenu;
 		SCasier casier;
-		SCasier encasiers[9];
+		SCasier encasiers[2];
+
+	/*Casier 1*/
+
+		SRouleau ro1;
+		ro1.longueur = 3.5;
+
+		SPalette pa1;
+		pa1.poids = 33;
+		strcpy_s (pa1.reference,  "158589551");
+
+		EEtat etatC1;
+		etatC1 = Palette;
+
+		UContenu cont1 ;
+		cont1.paletteCasier = pa1;
+		cont1.rouleauCasier = ro1;
+
+		SCasier c1;
+		c1.paletteCasier = pa1;
+		c1.etat = etatC1;
+		c1.px = 2;
+		c1.py = 3;
+
+	/*Casier 2*/
+
+		SRouleau ro2;
+		ro2.longueur = 4.777;
+
+		SPalette pa2;
+		pa2.poids = 55;
+		strcpy_s(pa2.reference, "158589551");
+
+		EEtat etatC2;
+		etatC2 = Palette;
+
+		UContenu cont2;
+		cont2.paletteCasier = pa2;
+		cont2.rouleauCasier = ro2;
+
+		SCasier c2;
+		c2.paletteCasier = pa2;
+		c2.etat = etatC2;
+		c2.px = 7;
+		c2.py = 8;
+
+
+		return 0;
 }
--- Transgerbeur/Transgerbeur.h
+++ Transgerbeur/Transgerbeur.h
@@ -1,22 +1,29 @@
+#include <string.h>
+
 enum EEtat {
 	Palette,
 	Rouleau,
 	Vide
 };
 
-union UContenu {
-	bool Palette;
-	bool Rouleau;
-};
-
 struct SPalette {
-
+	int poids;
+	char reference[20];
 };
 
 struct SRouleau {
+		float longueur;
+};
 
+union UContenu {
+	SPalette paletteCasier;
+	SRouleau rouleauCasier;
 };
 
 struct SCasier {
-
+	int px;
+	int py;
+	SPalette paletteCasier;
+	SRouleau rouleauCasier;
+	EEtat etat;
 };
