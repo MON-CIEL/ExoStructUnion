5bbbfcef8fa1851a3afbe87292495e19ed6a483b
MON-CIEL <mamonteirogarcia@gmail.com>
2024-12-30 18:20:19 +0100

    Commit   #5 "Capture d'ecran"

--- RGB/RGB.cpp
+++ RGB/RGB.cpp
@@ -1,5 +1,6 @@
 #include <stdlib.h> /* Lib Standard*/
 #include <iostream> /* Lib Cin , Cout */
+#include "RGB.h"
 using namespace std;
 
 // Nom du projet : TP5 Struct Enum Union
@@ -10,5 +11,27 @@ using namespace std;
 // Résumé :
 
 int main() {
+
+	/*
+		UColor colorVal;
+		colorVal.val = 100 * 256 * 256 + 15 * 256 + 130;
+		colorVal.components.g = 200;
+		cout << sizeof(colorVal) << endl;
+	*/
+
 	cout << endl << "Voici le porgramme RGB" << endl;
+
+	/* Declarations Variables */
+	Srgb valeursCouleurs;
+	
+	
+	cout << endl << "Veuillez saisir une valeur pour r en hexadecimale" << endl;
+	cout << endl << "Valeur saisit : ";
+	cin >> hex >> valeursCouleurs.r;
+	cout << endl << "Veuillez saisir une valeur pour g en hexadecimale" << endl;
+	cout << endl << "Valeur saisit : ";
+	cin >> hex >> valeursCouleurs.g;
+	cout << endl << "Veuillez saisir une valeur pour b en hexadecimale" << endl;
+	cout << endl << "Valeur saisit : ";
+	cin >> hex >> valeursCouleurs.b;
 }
---
+++ RGB/RGB.h
@@ -0,0 +1,11 @@
+struct Srgb {
+	unsigned char r;
+	unsigned char g;
+	unsigned char b;
+};
+
+union UColor {
+	unsigned int val;
+	Srgb components;
+	unsigned char tabCol[3];
+};
