b926f462c01e140c4b3467f6b5da83b6d07ee8ae
MON-CIEL <mamonteirogarcia@gmail.com>
2024-12-30 18:48:07 +0100

    Commit #6 "Creation du code de saisit et affcihage des valeurs hexadecimal"

--- RGB/RGB.cpp
+++ RGB/RGB.cpp
@@ -1,5 +1,6 @@
 #include <stdlib.h> /* Lib Standard*/
 #include <iostream> /* Lib Cin , Cout */
+#include <iomanip> /* Lib Set... , hex , dec , ...*/
 #include "RGB.h"
 using namespace std;
 
@@ -10,28 +11,80 @@ using namespace std;
 // Version du compilateur : (facultatif)
 // Résumé :
 
+bool verifier(unsigned char* var);
+bool verifier(unsigned char* var) {
+	if (cin.fail()) {
+
+		cout << endl << "Veuillez ressaisir la valeur saisit" << endl;
+		cin.clear();
+		cin.ignore(1000, '\n');
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
 int main() {
 
-	/*
-		UColor colorVal;
-		colorVal.val = 100 * 256 * 256 + 15 * 256 + 130;
-		colorVal.components.g = 200;
-		cout << sizeof(colorVal) << endl;
-	*/
+
 
 	cout << endl << "Voici le porgramme RGB" << endl;
 
 	/* Declarations Variables */
-	Srgb valeursCouleurs;
-	
-	
-	cout << endl << "Veuillez saisir une valeur pour r en hexadecimale" << endl;
-	cout << endl << "Valeur saisit : ";
-	cin >> hex >> valeursCouleurs.r;
-	cout << endl << "Veuillez saisir une valeur pour g en hexadecimale" << endl;
-	cout << endl << "Valeur saisit : ";
-	cin >> hex >> valeursCouleurs.g;
-	cout << endl << "Veuillez saisir une valeur pour b en hexadecimale" << endl;
-	cout << endl << "Valeur saisit : ";
-	cin >> hex >> valeursCouleurs.b;
+	Srgb valeursColeurs;
+	UColor colorVal;
+	int choix;
+	bool reccomencerChoix;
+
+	do {
+
+		cout << endl << "Voulez vous saisir chaque valeur (r , g , b) ou une valeur unique" << endl;
+		cout << endl << "	- Veuillez saisir 1 si vous voulez saisir chaque valeur " << endl;
+		cout << endl << "	- Veuillez saisir 2 si vous voulez saisir une valeur unique " << endl;
+		cout << endl << "Reponse : ";
+		cin >> choix;
+	} while (choix != 1 && choix != 2);
+
+	switch (choix) {
+	default:
+		break;
+
+	case 1: {
+
+		cout << endl << "Veuillez saisir une valeur pour r en hexadecimale" << endl;
+		cout << endl << "Valeur saisit : ";
+		do {
+			cin >> hex >> valeursColeurs.r;
+		} while (verifier(&valeursColeurs.r));
+
+		cout << endl << "Veuillez saisir une valeur pour g en hexadecimale" << endl;
+		cout << endl << "Valeur saisit : ";
+		do {
+			cin >> hex >> valeursColeurs.g;
+		} while (verifier(&valeursColeurs.g));
+
+		cout << endl << "Veuillez saisir une valeur pour b en hexadecimale" << endl;
+		cout << endl << "Valeur saisit : ";
+		do {
+			cin >> hex >> valeursColeurs.b;
+		} while (verifier(&valeursColeurs.b));
+
+
+		break;
+	}
+
+	case 2: {
+
+		for (int i = 0; i < 3; i++) {
+			cout << endl << "Veuillez saisir votre " << i + 1 << " valeur hexadecimale" << endl;
+			cout << endl << "Valeur saisit : ";
+			cin >> hex >> colorVal.tabCol[i];
+		}
+		colorVal.val = colorVal.components.r * 256 * 256 + colorVal.components.g * 256 + colorVal.components.b;
+
+		break;
+	}
+	}
+
 }
